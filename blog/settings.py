84b496b582d8b4f80136b4fa19a6914f1ade3bc6
oys2021 <opokuyawsarfo3@gmail.com>
2025-06-14 23:39:20 +0000

    chore: add static files settings

--- blog/settings.py
+++ blog/settings.py
@@ -55,6 +55,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
@@ -168,6 +169,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/profiles/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'profiles')
